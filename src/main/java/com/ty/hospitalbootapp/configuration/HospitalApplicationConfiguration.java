2df4fe567876a21656f021a51bf8cde4b4a4c833
Safikul99 <safikuli343@gmail.com>
2024-11-09 11:47:23 +0530

    first commit

--- src/main/java/com/ty/hospitalbootapp/configuration/HospitalApplicationConfiguration.java
+++ src/main/java/com/ty/hospitalbootapp/configuration/HospitalApplicationConfiguration.java
@@ -3,6 +3,7 @@ package com.ty.hospitalbootapp.configuration;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class HospitalApplicationConfiguration {
+	@Bean
 	public Docket getDocket() {
 		Contact contact = new Contact("Testyantra Global Team", "https://testyantraglobal.com/",
 				"test@testyantraglobal.com");
